2fa0fa8f3b97d407f13061670f272e7b1f3b7aba
Victor Valentim <jvvalentim.096@gmail.com>
2024-03-14 18:15:40 -0300

    bee-1023: done

--- 4-data-structures-and-libraries/bee-1023/index.js
+++ 4-data-structures-and-libraries/bee-1023/index.js
@@ -11,71 +11,68 @@ const input = `3
 2
 1 1
 3 2
-0 `;
+0`;
 // const input = require("fs").readFileSync("/dev/stdin", "utf8");
 const lines = input.split("\n");
 
-const parseProperty = (property) => {
-  let [residents, consumption] = property.split(" ");
+const parseCases = (totalProperties) => {
+  const city = {
+    propertyAvgConsumptionKeys: [],
+    residents: 0,
+    consumption: 0,
+    avgConsumption: 0,
+  };
 
-  residents = parseInt(residents);
-  consumption = parseInt(consumption);
+  let consumptionMap = new Map();
+  let properties = lines.slice(1, totalProperties + 1);
+
+  lines.splice(0, totalProperties + 1);
+
+  for (const property of properties) {
+    const values = property.split(" ");
+    const residents = parseInt(values[0]);
+    const consumption = parseInt(values[1]);
+    const avg = Math.floor(consumption / residents);
+
+    if (consumptionMap.has(avg)) {
+      consumptionMap.set(avg, consumptionMap.get(avg) + residents);
+    } else {
+      consumptionMap.set(avg, residents);
+      city.propertyAvgConsumptionKeys.push(avg);
+    }
+
+    city.residents += residents;
+    city.consumption += consumption;
+  }
+
+  city.propertyAvgConsumptionKeys = city.propertyAvgConsumptionKeys.sort((a, b) => a - b);
+  city.avgConsumption = city.consumption / city.residents;
 
   return {
-    residents,
-    consumption,
-    avgConsumption: Math.floor(consumption / residents),
+    propertiesAvgAsString: city.propertyAvgConsumptionKeys.map((key) => `${consumptionMap.get(key)}-${key}`).join(" "),
+    avgConsumption: city.avgConsumption,
   };
 };
 
-const parseCases = (index, amount, caseNum) => {
-  const nextCaseIndex = index + amount + 1;
-  const [start, end] = [index + 1, index + 1 + amount];
+let caseNum = 1;
+let totalProperties = parseInt(lines[0]);
 
-  const properties = lines
-    .slice(start, end)
-    .map((property) => parseProperty(property))
-    .sort((p1, p2) => p1.avgConsumption - p2.avgConsumption);
-
-  const city = properties.reduce(
-    (city, property) => {
-      city.propertiesAvgAsString.push(`${property.residents}-${property.avgConsumption}`);
-      city.residents += property.residents;
-      city.consumption += property.consumption;
-      city.avgConsumption = city.consumption / city.residents;
-
-      return city;
-    },
-    {
-      propertiesAvgAsString: [],
-      residents: 0,
-      consumption: 0,
-      avgConsumption: 0,
-    }
-  );
+while (totalProperties > 0) {
+  const result = parseCases(totalProperties);
 
   console.log(`Cidade# ${caseNum}:`);
-  console.log(city.propertiesAvgAsString.join(" "));
+  console.log(result.propertiesAvgAsString);
 
-  if (city.avgConsumption.toString().includes(".")) {
-    console.log(`Consumo medio: ${city.avgConsumption.toString().match(/^\d+(?:\.\d{0,2})?/)} m3.`);
+  if (result.avgConsumption.toString().includes(".")) {
+    console.log(`Consumo medio: ${result.avgConsumption.toString().match(/^\d+(?:\.\d{0,2})?/)} m3.`);
   } else {
-    console.log(`Consumo medio: ${city.avgConsumption}.00 m3.`);
+    console.log(`Consumo medio: ${result.avgConsumption}.00 m3.`);
   }
 
-  return nextCaseIndex;
-};
+  totalProperties = parseInt(lines[0]);
+  caseNum += 1;
 
-let caseNum = 0;
-let currCaseIndex = 0;
-let numProperties = parseInt(lines[currCaseIndex]);
-
-while (numProperties > 0) {
-  caseNum = caseNum + 1;
-  currCaseIndex = parseCases(currCaseIndex, numProperties, caseNum);
-  numProperties = parseInt(lines[currCaseIndex]);
-
-  if (numProperties === 0) {
+  if (totalProperties === 0) {
     break;
   }
 
